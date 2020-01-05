b108c10846211ff3fcdf3daeb06f1063c938298a
Alexey Makarenya <alexey@deli.su>
2020-01-05 22:59:31 +0100

    Simplified GL error messages

--- errors.cpp
+++ errors.cpp
@@ -14,14 +14,14 @@ std::string TGlewError::ErrorMessage(GLenum error, const std::string &context) {
 
 std::string TGlError::CodeDescription(GLenum code) {
     switch (code) {
-        case GL_INVALID_ENUM:return "Given when an enumeration parameter is not a legal enumeration for that function.";
-        case GL_INVALID_VALUE:return "Given when a value parameter is not a legal value for that function.";
-        case GL_INVALID_OPERATION:return "Given when the set of state for a command is not legal for the parameters given to that command.";
-        case GL_STACK_OVERFLOW:return "Given when a stack pushing operation cannot be done because it would overflow the limit of that stack's size.";
-        case GL_STACK_UNDERFLOW:return "Given when a stack popping operation cannot be done because the stack is already at its lowest point.";
-        case GL_OUT_OF_MEMORY:return "Given when performing an operation that can allocate memory, and the memory cannot be allocated.";
-        case GL_INVALID_FRAMEBUFFER_OPERATION:return "Given when doing anything that would attempt to read from or write/render to a framebuffer that is not complete.";
-        case GL_CONTEXT_LOST:return "Given if the OpenGL context has been lost, due to a graphics card reset.";
+        case GL_INVALID_ENUM:return "GL_INVALID_ENUM";
+        case GL_INVALID_VALUE:return "GL_INVALID_VALUE";
+        case GL_INVALID_OPERATION:return "GL_INVALID_OPERATION";
+        case GL_STACK_OVERFLOW:return "GL_STACK_OVERFLOW";
+        case GL_STACK_UNDERFLOW:return "GL_STACK_UNDERFLOW";
+        case GL_OUT_OF_MEMORY:return "GL_OUT_OF_MEMORY";
+        case GL_INVALID_FRAMEBUFFER_OPERATION:return "GL_INVALID_FRAMEBUFFER_OPERATION";
+        case GL_CONTEXT_LOST:return "GL_CONTEXT_LOST";
         default:return "Unknown error";
     }
 }
